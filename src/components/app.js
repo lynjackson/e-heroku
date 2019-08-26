fec7c4797cd5befcade3674303f067a858213dfa
bklyn <ljoconnor5@gmail.com>
2019-08-26 11:13:56 -0400

    completed mobile and tablet layouts.

--- src/components/app.js
+++ src/components/app.js
@@ -38,7 +38,7 @@ class App extends React.Component {
     }
 
     componentDidMount(){
-      let lato = new FontFace('Lato', 'url(../styles/fonts/Lato-Regular.ttf)', {
+      let lato = new FontFace('lato', 'url(../styles/fonts/Lato-Regular.ttf)', {
         style: 'normal',
         weight: '400'
       });
